1cb7caa0f7aa4ecdf226c8ca8478970f868413fa
Logan Saunders <lsaunders275@gmail.com>
2019-04-18 13:25:43 -0600

    tweaked the projects section; added language profiencies section

--- scripts/general.js
+++ scripts/general.js
@@ -26,7 +26,6 @@ document.getElementById("projects").addEventListener('mouseover', e => {
 });
 
 document.getElementById("projects").addEventListener('mouseout', e => {
-    console.log("left the projects");
     const projects = document.getElementById("projects").children;
     for (let i = 0; i < projects.length; i++) { // hide all the descriptions
         for (let j = 1; j < projects[i].children.length; j++) { // skip the video
@@ -48,17 +47,21 @@ function setWidthOfProjects(element) {
         const width = 100 / projects.length;
         for (let i = 0; i < projects.length; i++) {
             projects[i].style.width = width + "%";
+            projects[i].children[0].pause(); // pause the video if they aren't already
         }
     } else { // compensate for the hovered element
         const hoveredWidth = 75;
         element.style.width = hoveredWidth + "%";
+        // autoplay the video
+        element.children[0].play();
         // show the children of the element
-        for (let i = 0; i < element.children.length; i++) {
+        for (let i = 1; i < element.children.length; i++) {
             element.children[i].classList.remove("make-invisible");
         }
         const width = (100 - hoveredWidth) / (projects.length - 1);
         for (let i = 0; i < projects.length; i++) {
             if (projects[i] != element) {
+                projects[i].children[0].pause();
                 // hide the children if they aren't hidden already
                 for (let j = 1; j < projects[i].children.length; j++) { // skip the video
                     if (!projects[i].children[j].classList.contains("make-invisible")) {
